346fb08f2dfa19ef4a2940a8223fc7c855e81511
Sachin23051 <109645236+Sachin23051@users.noreply.github.com>
2024-09-20 16:01:31 +0530

    Add files via upload

--- pattern2.py
+++ pattern2.py
@@ -1,19 +1,15 @@
-#   1  2  3  4  5
-#   *  *  *  *  *
-#   11  12  13  14  15
-#   *  *  *  *  *
-#   21  22  23  24  25
-#   *  *  *  *  *
-
-n = 6
-num =1
-for i in range(1,n+1):
-    for j in range(1,6):
-        if i%2==0:
-            print("  *" , end="")
+n=18
+num=1
+while num<=n:
+    for i in range(1,11):
+        if i<6 and num <=n:
+            print( ' ', num,end='')
             num+=1
-        else:
-            print(" ",num,end="")
+        elif i > 5 and num <=n:
+            print("*" ,end ='')
             num+=1
-    # num+=1
+            
     print()
+
+        
+        
